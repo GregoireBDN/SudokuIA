d4c0a09bc69656cf6c44ff0eb565ac0225d9d6cf
GregoireBDN <gregoire.bodin04@gmail.com>
2025-02-01 08:58:52 +0100

    cration de l'algo de résolution

--- src/Grille.java
+++ src/Grille.java
@@ -8,114 +8,198 @@ public class Grille {
 	private Case[][] _cases;
 	private int _taille;
 	private int _nbCasesVides;
-	
-	
-	public Grille(int n){
+	private int _regionSize;
+
+	public Grille(int n) {
 		_taille = n;
-		_nbCasesVides = n*n;
+		_nbCasesVides = n * n;
+		_regionSize = (int) Math.sqrt(_taille);
 		_cases = new Case[n][n];
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
 				_cases[i][j] = new Case(i, j, n);
 			}
 		}
 	}
-	
-	public Grille(int n, int[][] grille){
+
+	public Grille(int n, int[][] grille) {
 		_taille = n;
-		_nbCasesVides = n*n;
+		_nbCasesVides = n * n;
 		_cases = new Case[n][n];
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
+		_regionSize = (int) Math.sqrt(_taille);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
 				_cases[i][j] = new Case(i, j, grille[i][j], n);
-				if(_cases[i][j].getVal() != 0){
-					_nbCasesVides --;
+				if (_cases[i][j].getVal() != 0) {
+					_nbCasesVides--;
 				}
 			}
 		}
 	}
-	
-	public Grille(Grille grille){
+
+	public Grille(Grille grille) {
 		_taille = grille._taille;
 		_nbCasesVides = grille._nbCasesVides;
+		_regionSize = grille._regionSize;
 		_cases = new Case[_taille][_taille];
-		for(int i=0; i<_taille; i++){
-			for(int j=0; j<_taille; j++){
+		for (int i = 0; i < _taille; i++) {
+			for (int j = 0; j < _taille; j++) {
 				_cases[i][j] = new Case(grille.getCase(i, j));
 			}
 		}
 	}
-	
-	
-	public Case getCase(int i, int j){
+
+	public Case getCase(int i, int j) {
 		return _cases[i][j];
 	}
-	
+
 	/*
 	 * Nombre de cases vides dans la grille.
 	 */
-	public int getNbCasesVides(){
+	public int getNbCasesVides() {
 		return _nbCasesVides;
 	}
-	
-	
+
 	/*
 	 * Fonction qui retourne une case vide de la grille, s'il en existe une.
 	 */
-	public Case getCasePossible(){
-		// TODO
-		Case casePoss = _cases[0][0];
-		return casePoss;
+	public Case getCasePossible() {
+		for (Case[] colonne : _cases) {
+			for (Case c : colonne) {
+				if (verifCase(c)) {
+					return c;
+				}
+			}
+		}
+		return null;
 	}
-	
-	
+
+	public Case getCasePossibleOptimized() {
+		for (Case[] colonne : _cases) {
+			for (Case c : colonne) {
+				if (verifCase(c)) {
+					return c;
+				}
+			}
+		}
+		return null;
+	}
+
 	/*
 	 * Fonction qui donne la valeur v à la case de mêmes coordonnées que la case c.
 	 */
-	public void setCase(Case c, int v){
-		// TODO
+	public void setCase(Case c, int v) {
+		_cases[c.getI()][c.getJ()].setVal(v);
+		_nbCasesVides--;
 	}
-	
-	
+
+	/*
+	 * Fonction qui retourne vrai si la case c est vide.
+	 */
+	public boolean verifCase(Case c) {
+		return c.getVal() == 0;
+	}
+
+	/*
+	 * Fonction qui retourne vrai si la valeur v peut être donnée à la colonne
+	 * colonne.
+	 */
+	public boolean verifColonne(int colonne, int valeur) {
+		for (int i = 0; i < _taille; i++) {
+			if (_cases[i][colonne].getVal() == valeur) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * Fonction qui retourne vrai si la valeur v peut être donnée à la ligne ligne.
+	 */
+	public boolean verifLigne(int ligne, int valeur) {
+		for (int j = 0; j < _taille; j++) {
+			if (_cases[ligne][j].getVal() == valeur) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * Fonction qui retourne vrai si la valeur v peut être donnée à la région
+	 * region.
+	 */
+	public boolean verifRegion(Case c, int valeur) {
+		int debutRegionI = c.getI() / _regionSize * _regionSize;
+		int debutRegionJ = c.getJ() / _regionSize * _regionSize;
+		for (int i = debutRegionI; i < debutRegionI + _regionSize; i++) {
+			for (int j = debutRegionJ; j < debutRegionJ + _regionSize; j++) {
+				if (_cases[i][j].getVal() == valeur) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 	/*
 	 * Fonction qui retourne vrai si la valeur v peut être donnée à la case c
 	 * c'est-à-dire si la grille respecte toujours les contraintes du Sudoku.
 	 */
-	public boolean casePossible(Case c, int v){
-		// TODO
+	public boolean casePossible(Case c, int v) {
+		Case caseGrille = getCase(c.getI(), c.getJ());
+		// Vérifie si la case est déjà remplie
+		if (!verifCase(caseGrille)) {
+			return false;
+		}
+
+		// Vérification de la ligne
+		if (!verifLigne(caseGrille.getI(), v)) {
+			return false;
+		}
+
+		// Vérification de la colonne
+		if (!verifColonne(caseGrille.getJ(), v)) {
+			return false;
+		}
+
+		// Vérification de la région
+		if (!verifRegion(caseGrille, v)) {
+			return false;
+		}
+
 		return true;
 	}
-	
+
 	/*
 	 * Affichage.
 	 */
-	public void afficheGrille(){
+	public void afficheGrille() {
 		int v;
-		int dim = (int)Math.sqrt(_taille);
-		for(int i=0; i<_taille; i++){
-			if(i%dim == 0){
+		int dim = (int) Math.sqrt(_taille);
+		for (int i = 0; i < _taille; i++) {
+			if (i % dim == 0) {
 				System.out.print(" ");
-				for(int k=0; k<=_taille; k++)
+				for (int k = 0; k <= _taille; k++)
 					System.out.print("--");
 				System.out.println();
 			}
-			for(int j=0; j<_taille; j++){
-				if(j%dim == 0){
+			for (int j = 0; j < _taille; j++) {
+				if (j % dim == 0) {
 					System.out.print("|");
 				}
 				v = _cases[i][j].getVal();
-				if(v == 0){
-					System.out.print("  ");					
-				}
-				else{
+				if (v == 0) {
+					System.out.print("  ");
+				} else {
 					System.out.print(v + " ");
 				}
 			}
 			System.out.println("|");
 		}
 		System.out.print(" ");
-		for(int k=0; k<=_taille; k++)
+		for (int k = 0; k <= _taille; k++)
 			System.out.print("--");
-		System.out.println();		
+		System.out.println();
 	}
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,3 +1,4 @@
+
 /*
  * TP 1 - IA Informatique : Sudoku
  * 
@@ -7,43 +8,42 @@ import java.util.Stack;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-
 public class Sudoku {
 	public static final int TAILLE = 9;
 	private static int[][] grille1 = {
-			{0,8,0,4,0,2,0,6,0},
-			{0,3,4,0,0,0,9,1,0},
-			{9,6,0,0,0,0,0,8,4},
-			{0,0,0,2,1,6,0,0,0},
-			{0,0,0,0,0,0,0,0,0},
-			{0,0,0,3,5,7,0,0,0},
-			{8,4,0,0,0,0,0,7,5},
-			{0,2,6,0,0,0,1,3,0},
-			{0,9,0,7,0,1,0,4,0}
-		};
+			{ 0, 8, 0, 4, 0, 2, 0, 6, 0 },
+			{ 0, 3, 4, 0, 0, 0, 9, 1, 0 },
+			{ 9, 6, 0, 0, 0, 0, 0, 8, 4 },
+			{ 0, 0, 0, 2, 1, 6, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 3, 5, 7, 0, 0, 0 },
+			{ 8, 4, 0, 0, 0, 0, 0, 7, 5 },
+			{ 0, 2, 6, 0, 0, 0, 1, 3, 0 },
+			{ 0, 9, 0, 7, 0, 1, 0, 4, 0 }
+	};
 	private static int[][] grilleHardest = {
-			{1,0,0,0,0,7,0,9,0},
-			{0,3,0,0,2,0,0,0,8},
-			{0,0,9,6,0,0,5,0,0},
-			{0,0,5,3,0,0,9,0,0},
-			{0,1,0,0,8,0,0,0,2},
-			{6,0,0,0,0,4,0,0,0},
-			{3,0,0,0,0,0,0,1,0},
-			{0,4,0,0,0,0,0,0,7},
-			{0,0,7,0,0,0,3,0,0}
-		};
+			{ 1, 0, 0, 0, 0, 7, 0, 9, 0 },
+			{ 0, 3, 0, 0, 2, 0, 0, 0, 8 },
+			{ 0, 0, 9, 6, 0, 0, 5, 0, 0 },
+			{ 0, 0, 5, 3, 0, 0, 9, 0, 0 },
+			{ 0, 1, 0, 0, 8, 0, 0, 0, 2 },
+			{ 6, 0, 0, 0, 0, 4, 0, 0, 0 },
+			{ 3, 0, 0, 0, 0, 0, 0, 1, 0 },
+			{ 0, 4, 0, 0, 0, 0, 0, 0, 7 },
+			{ 0, 0, 7, 0, 0, 0, 3, 0, 0 }
+	};
 	private static int[][] grilleNoSolution = {
-			{0,8,0,4,0,2,0,6,0},
-			{0,3,4,0,0,0,9,1,0},
-			{9,6,0,0,0,0,0,8,4},
-			{0,0,0,2,1,6,0,0,0},
-			{0,0,0,0,0,0,0,0,0},
-			{0,0,0,3,5,7,0,0,0},
-			{8,4,0,0,0,0,0,7,5},
-			{0,2,6,0,0,0,1,3,0},
-			{0,9,0,7,0,1,0,4,9}
-		};
-	
+			{ 0, 8, 0, 4, 0, 2, 0, 6, 0 },
+			{ 0, 3, 4, 0, 0, 0, 9, 1, 0 },
+			{ 9, 6, 0, 0, 0, 0, 0, 8, 4 },
+			{ 0, 0, 0, 2, 1, 6, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 3, 5, 7, 0, 0, 0 },
+			{ 8, 4, 0, 0, 0, 0, 0, 7, 5 },
+			{ 0, 2, 6, 0, 0, 0, 1, 3, 0 },
+			{ 0, 9, 0, 7, 0, 1, 0, 4, 9 }
+	};
+
 	public static void main(String[] args) {
 		System.out.println("Sudoku de bon matin...");
 
@@ -52,44 +52,58 @@ public class Sudoku {
 		grilles.put("1", new Grille(grille1.length, grille1));
 		grilles.put("difficile", new Grille(grilleHardest.length, grilleHardest));
 		grilles.put("sans solution", new Grille(grilleNoSolution.length, grilleNoSolution));
-		
+
 		// Résolution des grilles
 		for (Entry<String, Grille> entry : grilles.entrySet()) {
 			System.out.println("\n========================================================================");
 			System.out.println("Je vais essayer de résoudre la grille " + entry.getKey() + ".");
-			
+
 			Grille grille = entry.getValue();
 			grille.afficheGrille();
-		
+
 			// On place la grille dans une pile pour la résoudre
 			Stack<Grille> pile = new Stack<>();
 			pile.push(grille);
-			
+
 			boolean hasSolution = resoudreSudoku(pile);
-			
+
 			if (hasSolution) {
 				System.out.println("La grille a été résolue.");
 				Grille grilleResul = pile.peek();
 				grilleResul.afficheGrille();
-			}
-			else {
+			} else {
 				System.out.println("Aucune solution n'a été trouvée.");
 			}
 		}
-	}//main
-	
-	
+	}// main
+
 	/*
-	 * Fonction récursive qui recherche la solution, 
+	 * Fonction récursive qui recherche la solution,
 	 * en utilisant éventuellement des retours-arrière.
 	 * 
 	 * La grille iniitiale doit être placée dans pileGrilles.
-	 * Retourne true si une solution a été trouvée, 
+	 * Retourne true si une solution a été trouvée,
 	 * la solution se trouve au sommet de pileGrilles.
 	 */
-	public static boolean resoudreSudoku(Stack<Grille> pileGrilles){
-		// TODO
+	public static boolean resoudreSudoku(Stack<Grille> pileGrilles) {
+		Grille grille = pileGrilles.peek();
+		if (grille.getNbCasesVides() == 0) {
+			return true;
+		}
+		Case caseVide = grille.getCasePossible();
+		for (int i = 1; i <= TAILLE; i++) {
+			if (grille.casePossible(caseVide, i)) {
+				Grille grilleSuivante = new Grille(grille);
+				grilleSuivante.setCase(caseVide, i);
+				pileGrilles.push(grilleSuivante);
+				if (resoudreSudoku(pileGrilles)) {
+					return true;
+				}
+				pileGrilles.pop();
+			}
+		}
+
 		return false;
-	}//resoudreSudoku
+	}// resoudreSudoku
 
 }
